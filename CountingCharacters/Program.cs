e0b5929a5ac9795505cb6b132bea34fe3ed3aea0
Tim Lucas <tllucas1s@gmail.com>
2018-05-12 19:03:23 -0500

    Finalized studio, tested and working.

--- CountingCharacters/Program.cs
+++ CountingCharacters/Program.cs
@@ -10,6 +10,29 @@ namespace CountingCharacters
     {
         static void Main(string[] args)
         {
+            string input = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            Dictionary<char, int> Chars = new Dictionary<char, int>();
+            Dictionary<char, int> result = CountingCharacters(input);
+            foreach (KeyValuePair<char, int> kv in Chars)
+                Console.WriteLine("{0}; {1}", kv.Key, kv.Value);
+            Console.ReadLine();
+
+             Dictionary<char, int> CountingCharacters(string text)
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (Chars.ContainsKey(text[i]))
+                    {
+                        Chars[text[i]] += 1;
+                    }
+                    else
+                    {
+                        Chars.Add(text[i], 1);
+                    }
+                }
+
+                return Chars;
+            }
         }
     }
 }
